271d98ceb7f799e5298276da5c8bfb3a40095fa7
Eric van Riet Paap <ericvrp@gmail.com>
2013-04-28 22:26:27 +0200

    Add pvoutput.org support initial version

--- PowerToThePeople.py
+++ PowerToThePeople.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-from time import time, sleep
+from requests import get
+from time import time, sleep, strftime
 
-
-# pvoutput.org-support3
+try:
+	from config import *
+except ImportError:
+	from defaults import *
+	print 'Warning! copy defaults.py to config.py and edit that file!'
 
 LDR_PIN              = 4	#LDR = Light Dependent Resistore
 MAX_MEASUREMENTS     = 9000	#assume there is no led flashing with this little light
 IMPRESSION_THRESHOLD = 0.9	#if measurements < MAX_MEASUREMENTS * IMPRESSION_THRESHOLD: then we assume led flashed
-IMPRESSIONS_PER_kWh  = 1000	#my electricity meter flashes a light this many times per kWh
+#IMPRESSIONS_PER_kWh  = 1000	#my electricity meter flashes a light this many times per kWh
+PVOUTPUT_INTERVAL    = 300	#5 minutes between sending updates
 
 
 def	RCtime():
@@ -32,21 +37,36 @@ def	main():
 	GPIO.setwarnings(False)	#shut up!
 	GPIO.setmode(GPIO.BCM)	#use Broadcom GPIO references (instead of board references)
 
+	lastPvOutputTime = time()
 	lastLedFlashTime = time()	#first impression duration will be inaccurate
 	watt = 0
 
 	while True:
 		nMeasurements, duration = RCtime()
 		duration = int(duration * 100000.0) # Low values for much light
+		now = time()
 
 		if nMeasurements < MAX_MEASUREMENTS * IMPRESSION_THRESHOLD:
-			t = time()
-			impressionDuration = t - lastLedFlashTime
+			impressionDuration = now - lastLedFlashTime
 			lastWatt = watt
 			watt = 3600 / impressionDuration
-			lastLedFlashTime = t
+			lastLedFlashTime = now
+
 			print 'Impression took %.1f seconds (%d Watt) [%d Watt change]' %\
 				(impressionDuration, watt, watt - lastWatt)
+
+			if now >= lastPvOutputTime + PVOUTPUT_INTERVAL:
+				payload = {
+					'key' : pvoutput_key,
+					'sid' : pvoutput_sid,
+					'd'   : strftime('%Y%m%d'),
+					't'   : strftime('%H:%M'),
+					'v4'  : watt
+					}
+				r = get('http://pvoutput.org/service/r2/addstatus.jsp', params=payload)
+				#XXX post current power consumption for the moment
+				lastPvOutputTime = now
+				print 'Sent power consumption value to pvoutput.org (response %d)' % r.status_code
 
 		#print '%4d measurements in %5d time units (%d Watt) [%d Watt change]' %\
 		#	(nMeasurements, duration, watt, watt - lastWatt)
---
+++ defaults.py
@@ -0,0 +1,5 @@
+#
+# Copy this file to config.py and insert to correct values there!
+#
+pvoutput_key = '<your api key from pvoutput.org goes here>'
+pvoutput_sid = '<your system id goes here>'
