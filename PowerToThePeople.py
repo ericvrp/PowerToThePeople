110e06bffb282cacfd8d13095a29b6a1d098fd7f
Eric van Riet Paap <ericvrp@gmail.com>
2013-04-28 13:37:48 +0200

    Compute wattage

--- PowerToThePeople.py
+++ PowerToThePeople.py
@@ -4,9 +4,10 @@ import RPi.GPIO as GPIO
 from time import time, sleep
 
 
-LDR_PIN             = 4		#LDR = Light Dependent Resistore
-MAX_MEASUREMENTS    = 1999	#assume there is no led flashing with this little light
-IMPRESSIONS_PER_kWh = 1000	#my electricity meter flashes a light this many times per kWh
+LDR_PIN              = 4	#LDR = Light Dependent Resistore
+MAX_MEASUREMENTS     = 9000	#assume there is no led flashing with this little light
+IMPRESSION_THRESHOLD = 0.9	#if measurements < MAX_MEASUREMENTS * IMPRESSION_THRESHOLD: then we assume led flashed
+IMPRESSIONS_PER_kWh  = 1000	#my electricity meter flashes a light this many times per kWh
 
 
 def	RCtime():
@@ -29,10 +30,25 @@ def	main():
 	GPIO.setwarnings(False)	#shut up!
 	GPIO.setmode(GPIO.BCM)	#use Broadcom GPIO references (instead of board references)
 
+	lastLedFlashTime = time()	#first impression duration will be inaccurate
+	watt = 0
+
 	while True:
 		nMeasurements, duration = RCtime()
 		duration = int(duration * 100000.0) # Low values for much light
-		print '%4d measurements in %4d time units' % (nMeasurements, duration)
+
+		if nMeasurements < MAX_MEASUREMENTS * IMPRESSION_THRESHOLD:
+			t = time()
+			impressionDuration = t - lastLedFlashTime
+			lastWatt = watt
+			watt = 3600 / impressionDuration
+			lastLedFlashTime = t
+			print 'Impression took %.1f seconds (%d Watt) [%d Watt change]' %\
+				(impressionDuration, watt, watt - lastWatt)
+
+		#print '%4d measurements in %5d time units (%d Watt) [%d Watt change]' %\
+		#	(nMeasurements, duration, watt, watt - lastWatt)
 
 if __name__ == '__main__':
 	main()
+
