b063b2877bddd5165aa71d7d7ebc475ac1370e82
Eric van Riet Paap <ericvrp@gmail.com>
2013-04-30 13:49:12 +0200

    Post wattage averages now

--- PowerToThePeople.py
+++ PowerToThePeople.py
@@ -19,27 +19,32 @@ def	main():
 	ser.readline()	#Skip first led flash to get a proper duration after this
 
 	lastPvOutputTime = lastLedFlashTime = time()	#first impression duration will be inaccurate
+	nLedFlashes = 0
 
 	while True:
 		s = ser.readline()
-		#print '***', s,
+		#print 'Arduino: ', s,
 
 		now = time()
 		watt = 3600 / (now - lastLedFlashTime)
 		lastLedFlashTime = now
+		nLedFlashes += 1
 
 		print '%s : %4d Watt' % (asctime(), watt)
 
 		if now >= lastPvOutputTime + PVOUTPUT_INTERVAL: #XXX should post average power consumption
+			watt_average = nLedFlashes * 3600 / (now - lastPvOutputTime)
+		 	#print 'Watt Average %d' % watt_average
 			payload = {
 				'key' : pvoutput_key,
 				'sid' : pvoutput_sid,
 				'd'   : strftime('%Y%m%d'),
 				't'   : strftime('%H:%M'),
-				'v4'  : watt
+				'v4'  : watt_average
 				}
 			r = get('http://pvoutput.org/service/r2/addstatus.jsp', params=payload)
 			lastPvOutputTime = now
+			nLedFlashes = 0
 
 
 if __name__ == '__main__':
