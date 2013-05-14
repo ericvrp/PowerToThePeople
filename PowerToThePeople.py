08b0eccec79482189e7ba172e49afe68141d58d3
Eric van Riet Paap <ericvrp@gmail.com>
2013-05-14 20:06:50 +0200

    - Support gpio pin input beside the serial input that we already supported. This allows for a much simpler setup without an additional Arduino.
    - To make it even simpler we now use a software pull down resistor for the light dependent resistor (LDR). So this LDR can simple be put betwee the gpio pin and vcc.
    
    - Some conventient options were added to the config file (default.py)

--- PowerToThePeople.py
+++ PowerToThePeople.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-import serial
 from requests import get
 from requests.exceptions import Timeout, ConnectionError
 from time import time, strftime, asctime
-from sys import stdout
 from subprocess import check_output
 
 try:
@@ -13,21 +11,37 @@ except ImportError:
 	from defaults import *
 	print 'Warning! copy defaults.py to config.py and edit that file!'
 
-PVOUTPUT_INTERVAL = 300		#5 minutes between sending updates
-
 
 def	main():
-	usbDevice = check_output('ls /dev/ttyACM*', shell=True).strip()
-	ser = serial.Serial(usbDevice, 115200)
-	ser.flushInput()
-	ser.readline()	#Skip first led flash to get a proper duration after this
+	if source == 'serial':
+		import serial
 
+		usbDevice = check_output('ls /dev/ttyACM*', shell=True).strip()
+		ser = serial.Serial(usbDevice, 115200)
+		ser.flushInput()
+
+		def waitForLedFlash():
+			return ser.readline()
+
+	else:	#source = gpio pin number (simply connect gpio pin to 5V because we use a pulldown resistor from software)
+		import RPi.GPIO as GPIO
+
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(source, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		#GPIO.add_event_detect(source, GPIO.RISING, signalRising, 100)      #100ms debounce time
+
+		def waitForLedFlash():
+			GPIO.wait_for_edge(source, GPIO.RISING)
+			GPIO.wait_for_edge(source, GPIO.FALLING)	#poor mens debounce
+			return '<led flashed>'
+
+	waitForLedFlash()	#Skip first led flash to get a proper duration after this
 	lastPvOutputTime = lastLedFlashTime = time()	#first impression duration will be inaccurate
 	nLedFlashes = 0
 
 	while True:
-		s = ser.readline()
-		#print 'Arduino: ', s,
+		s = waitForLedFlash()
+		#print s
 
 		now = time()
 		current_usage = '%s : %4d Watt' % (asctime(), 3600 / (now - lastLedFlashTime))
@@ -36,11 +50,12 @@ def	main():
 
 		print current_usage
 		try:
-			r = get('http://127.0.0.1:8083/watt/' + current_usage, timeout=1.0)	#update webcache
+			if webcache_enabled:
+				get('http://127.0.0.1:8083/watt/' + current_usage, timeout=1.0)	#update webcache
 		except Timeout:
 			print 'Warning: webcache update failed'
 
-		if now >= lastPvOutputTime + PVOUTPUT_INTERVAL: #XXX should post average power consumption
+		if pvoutput_interval and now >= lastPvOutputTime + pvoutput_interval:
 			watt_average = nLedFlashes * 3600 / (now - lastPvOutputTime)
 		 	#print 'Watt Average %d' % watt_average
 			payload = {
@@ -51,15 +66,13 @@ def	main():
 				'v4'  : watt_average
 				}
 			try:
-				r = get('http://pvoutput.org/service/r2/addstatus.jsp', params=payload, timeout=5.0)
+				get('http://pvoutput.org/service/r2/addstatus.jsp', params=payload, timeout=5.0)
 			except ConnectionError:
 				print 'Warning: pvoutput update failed'
 			lastPvOutputTime = now
 			nLedFlashes = 0
 
-		stdout.flush()
-
 
 if __name__ == '__main__':
 	main()
-
+	#GPIO.cleanup()
--- defaults.py
+++ defaults.py
@@ -1,5 +1,10 @@
 #
 # Copy this file to config.py and insert to correct values there!
 #
-pvoutput_key = '<your api key from pvoutput.org goes here>'
-pvoutput_sid = '<your system id goes here>'
+source            = <gpio pin number> or <'serial'>
+
+webcache_enabled  = False
+
+pvoutput_interval = 0		#in seconds (or 0 to disable pvoutput)
+pvoutput_key      = '<your api key from pvoutput.org goes here>'
+pvoutput_sid      = '<your system id goes here>'
