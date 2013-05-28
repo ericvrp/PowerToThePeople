cf9450a64cb16df6d5d4b7a62d405506b1438536
Eric van Riet Paap <ericvrp@gmail.com>
2013-05-28 11:06:20 +0200

    Cleaning up the directory structure

--- site/PowerToThePeople.js
+++ site/PowerToThePeople.js
@@ -30,7 +30,89 @@ if (Meteor.isClient) {
 		}
 		return kWh.toFixed(1);
 	}
-}
+	/*
+    Template.map.rendered = function () {
+       // our d3 code goes here
+	   this.node = this.find('#video-map');
+   
+	   var yScale = d3.scale.linear()
+	      .domain([0, 4])
+	      .range([10, 60]);
+
+	  //var xScale = d3.scale.linear()
+	  //    .domain([0, self.duration])
+	  //    .range([0, $(self.timelineWrapper).width()]);
+
+	  var xScale = d3.scale.linear()
+	      .domain([0, 200])
+	      .range([0, 400]);
+
+	  if (! self.drawTimeline) {
+	      self.drawTimeline = Meteor.autorun(function() {
+
+	        var subtitles = ['Eric', 'van', 'Riet', 'Paap']; //Subtitles.find().fetch(); 
+
+	        self.captions = d3.select(self.node)
+	          .select('.caption-spans')
+	          .selectAll('rect')
+	          .data(subtitles, function (sub) {
+	            return sub._id; 
+	          });
+
+	        drawTimeline(); 
+
+	      });
+	    }
+		
+	    var drawTimeline = function(){
+
+	        // Append new captions
+	        drawSubs(self.captions.enter().append('rect'));
+
+	        // Update changed captions
+	        drawSubs(self.captions.transition().duration(400));
+
+	        // Remove captions
+	        self.captions
+	          .exit()
+	          .transition()
+	          .duration(400)
+	          .style('opacity', 0)
+	          .remove(); 
+	    }
+
+
+	    // Sets the attributes of each new or changed caption
+	    var drawSubs = function (caption) {
+	      caption
+	        .attr('data-id', function (cap) { return cap._id; })
+	        .attr('class', 'timelineEvent')
+	        .attr('fill', function (cap) { 
+
+	          // Provide colour warnings if too fast rate / second
+	          var rate = getRatio(cap);
+	          if (rate &lt;= 2.3)
+	            return '#50ddfb'; // it's good
+	          else if (rate &gt; 2.3 &amp;&amp; rate &lt; 3.1)
+	            return '#fbb450'; // warning
+	          else
+	            return '#ea8787'; // danger
+	        })
+	        .attr('x', function (cap) { return xScale(cap.startTime); })
+	        .attr('y', function (cap) { return '-' + yScale(getRatio(cap)); })
+	        .attr('width', function (cap) { 
+	          return xScale(cap.endTime) - xScale(cap.startTime)
+	        })
+	        .attr('height', function (cap) {
+	          return yScale(getRatio(cap)); 
+	        });
+	    };
+
+		Template.map.destroyed = function () {
+		    this.drawTimeline.stop(); 
+		  };
+     }*/
+ }
 
 if (Meteor.isServer) {
 	Meteor.startup(function () {
---
+++ site/server/README.txt
@@ -0,0 +1,1 @@
+Files here are only run on the server and aren’t available on the client.
