b747774d196f47d3aef7f02caf75fe5ff4c18427
Eric van Riet Paap <ericvrp@gmail.com>
2013-05-28 16:28:12 +0200

    Display a comparison between the kWh during the last 24 hours, the previous 24 hours and 24 hours a week ago

--- site/client/js/home.js
+++ site/client/js/home.js
@@ -4,12 +4,16 @@ Meteor.subscribe('MyWattage');
 Template.Power.watt = function() {
 	var	currentWattage = CurrentWattage.findOne({userId: Meteor.userId()});
 	if (!currentWattage)	return '';
-	return new Date(currentWattage.createdAt) + " : " + currentWattage.watt;
+	return currentWattage.watt + " Watt : " + new Date(currentWattage.createdAt);
 }
 
-Template.Energy.kWhLast24Hours = function() {
-	var _24hoursago = new Date().getTime() - (3600 * 24 * 1000);
-	var	samples = Wattage.find({userId: Meteor.userId(), createdAt: {$gte: _24hoursago}}).fetch(); //We could return only the kWh but that's for later
+Template.Energy.kWhDuring24Hours = function(daysAgo) {
+	var _24hours = 3600 * 24 * 1000;	//in milliseconds
+	var end      = new Date().getTime() - daysAgo * _24hours;
+	var begin    = end - _24hours;
+	var	samples  = Wattage.find({userId: Meteor.userId(), $and: [
+		{createdAt: {$gte: begin}}, 
+		{createdAt: {$lt: end}}]}).fetch(); //We could return only the kWh but that's for later
 	var kWh = 0.0;
 	for (n in samples) {
 		kWh += samples[n].kWh;
