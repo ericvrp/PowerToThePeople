5f054f7ff0d06c5293e64f1fcf6a8ce31d6cae1f
Eric van Riet Paap <ericvrp@gmail.com>
2013-05-30 09:35:28 +0200

    Playing around with bootstrap and adding d3 + raphael visualisations to see what works best

--- site/common/common.js
+++ site/common/common.js
@@ -2,6 +2,7 @@
 
 CurrentWattage = new Meteor.Collection("CurrentWattage");
 Wattage        = new Meteor.Collection("Wattage");
+Players        = new Meteor.Collection("Players"); //used by the d3 demo code
 
 log = function(s) {
 	console.log(s);
--- site/server/model.js
+++ site/server/model.js
@@ -1,3 +1,13 @@
+Players.allow({
+	insert: function() { return true; },
+	update: function() { return true; },
+	remove: function() { return true; }
+});
+
+Meteor.publish('AllPlayers', function() {
+	return Players.find();
+});
+
 Meteor.publish('MyCurrentWattage', function() {
 	return CurrentWattage.find({userId: this.userId});
 });
@@ -8,4 +18,15 @@ Meteor.publish('MyWattage', function() {
 
 Meteor.startup(function() {
 	//one time only server code goes here
+    if (Players.find().count() === 0) {
+      var names = ["Ada Lovelace",
+                   "Grace Hopper",
+                   "Marie Curie",
+                   "Carl Friedrich Gauss",
+                   "Nikola Tesla",
+                   "Claude Shannon"];
+      for (var i = 0; i < names.length; i++)
+        Players.insert({name: names[i], score: Math.floor(Math.random()*10)*5});
+    }
 });
+
